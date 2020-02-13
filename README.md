502e96aef44ca7068dbc229c7a335804042e7a87
kswanson25 <katieswanson259@gmail.com>
2020-02-13 00:52:16 -0800

    Create RESTful API Calls

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ npm run seed
 npm dev:build
 npm dev:start
 currently on: localhost:3009
+
+
+Endpoints       |  Type   |   Ops
+-------------------------------------------------------
+'/details/:id'  -  GET    -   Gets data for an item
+'/details/'     -  POST   -   Adds data for a new item
+'details/:id'   -  PUT    -   Updates an existing item
+'/details/:id'  -  DELETE -   Deletes an exiting item
+
--- server/app.js
+++ server/app.js
@@ -36,4 +36,52 @@ app.get('/details/:id', (req, res) => {
   });
 });
 
+//POST - create new item
+  //want to post new request body
+app.post('/details', (req, res) => {
+  const newItemData = [];
+  for (let i = 0; i < req.body.length; i += 1) {
+  newItemData = db.push(req.body[i]);
+    //or need to make a new instance?
+  }
+  db.insertMany(newItemData, (err, result) => {
+    if (error) {
+      console.log('POST REQ ERROR: ', error);
+      res.sendStatus(404);
+    } else {
+      res.json(res)
+    }
+  })
+});
+
+//PUT - update an existing item
+app.put('details/:id', (req, res) => {
+  const itemId = req.params.id;
+  //the experience id        the updates in the req body
+  db.updateOne({id: itemId}, req.body, (error, result) => {
+    if (error) {
+      console.log('PUT REQ ERROR: ', error);
+      res.sendStatus(404); //not found - can't update
+    } else {
+      //I think I have to convert response to json? not using axios
+      console.log('the res.json(result) of put req is ', res.json(result));
+      res.json(result);
+    };
+  })
+})
+
+//DELETE - delete an item
+app.delete('/details/:id', (req, res) => {
+  const itemId = req.params.id;
+  dd.DeleteOne({id: itemId}, (error, result) => {
+    if (error) {
+      console.log('DELETE REQ ERROR: ', error);
+      res.sendStatus(404); //cant find - cant delete
+    } else {
+      console.log('the res.json(result) "', res.json(result), '" was deleted');
+      res.json(result);
+    }
+  });
+});
+
 module.exports = app;
